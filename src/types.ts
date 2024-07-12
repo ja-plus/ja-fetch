583013c16acd7c3bbf6fe8b10cc2662cc3e68c58
JA+ <601286825@qq.com>
2024-07-12 14:06:28 +0800

    update

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,13 @@
-export interface JaFetchRequestInit extends RequestInit {
+export type ResponseType = 'text' | 'blob' | 'arraybuffer' | 'response';
+
+export interface JaFetchRequestInit<T = ResponseType> extends RequestInit {
   /**not JSON.stringify(body) */
   rawBody?: boolean;
   /**基本url */
   baseURL?: string;
   /**url请求参数 */
   params?: any;
-  responseType?: 'text' | 'blob' | 'arraybuffer' | 'response';
+  responseType?: T;
   body?: any | BodyInit;
   /**可在init中传任意自定义字段 */
   [k: string]: any;
